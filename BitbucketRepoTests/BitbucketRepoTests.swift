30218b5e774b3a12395664a56ac913ce5917efbe
KoingDev <sea.saingkoing@gmail.com>
2020-09-02 12:09:09 +0900

    ⚙️ Init project

---
+++ BitbucketRepoTests/BitbucketRepoTests.swift
@@ -0,0 +1,34 @@
+//
+//  BitbucketRepoTests.swift
+//  BitbucketRepoTests
+//
+//  Created by KoingDev on 9/2/20.
+//  Copyright © 2020 koing.dev. All rights reserved.
+//
+
+import XCTest
+@testable import BitbucketRepo
+
+class BitbucketRepoTests: XCTestCase {
+
+    override func setUp() {
+        // Put setup code here. This method is called before the invocation of each test method in the class.
+    }
+
+    override func tearDown() {
+        // Put teardown code here. This method is called after the invocation of each test method in the class.
+    }
+
+    func testExample() {
+        // This is an example of a functional test case.
+        // Use XCTAssert and related functions to verify your tests produce the correct results.
+    }
+
+    func testPerformanceExample() {
+        // This is an example of a performance test case.
+        self.measure {
+            // Put the code you want to measure the time of here.
+        }
+    }
+
+}
