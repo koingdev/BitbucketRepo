dd3740b12dd2ea58d08702935308fea19aa052db
KoingDev <sea.saingkoing@gmail.com>
2020-09-02 18:02:10 +0900

    ⚙️ Add more test assertion

--- BitbucketRepoTests/BitbucketTests.swift
+++ BitbucketRepoTests/BitbucketTests.swift
@@ -25,6 +25,10 @@ class BitbucketTests: XCTestCase {
         
         // Then
         XCTAssertEqual(bitbucket.name, "Design Pattern", "Name should be Design Pattern!")
+        XCTAssertEqual(bitbucket.description, "test_description", "Description should be test_description!")
+        XCTAssertEqual(bitbucket.type, "repository", "Type should be repository!")
+        XCTAssertEqual(bitbucket.size, 100, "Size should be 100!")
+        XCTAssertEqual(bitbucket.website, "test_web_site", "Website should be test_web_site!")
         XCTAssertEqual(bitbucket.language, "Swift", "Language should be Swift!")
         XCTAssertEqual(bitbucket.ownerDisplayName, "KoingDev", "Display name should be KoingDev!")
         XCTAssertEqual(bitbucket.ownerType, "user", "Type should be user!")
@@ -45,8 +49,14 @@ class BitbucketTests: XCTestCase {
         let bitbucket = Bitbucket.parse(json: json)
         
         // Then
-        XCTAssertTrue(bitbucket.ownerDisplayName.isEmpty, "Display name should be empty!")
+        XCTAssertTrue(bitbucket.name.isEmpty, "Name should be empty!")
+        XCTAssertTrue(bitbucket.description.isEmpty, "Description should be empty!")
         XCTAssertTrue(bitbucket.type.isEmpty, "Type should be empty!")
+        XCTAssertTrue(bitbucket.size == 0, "Type should be 0!")
+        XCTAssertTrue(bitbucket.website.isEmpty, "Website should be empty!")
+        XCTAssertTrue(bitbucket.language.isEmpty, "Language should be empty!")
+        XCTAssertTrue(bitbucket.ownerDisplayName.isEmpty, "Display name should be empty!")
+        XCTAssertTrue(bitbucket.ownerType.isEmpty, "Type should be empty!")
         XCTAssertTrue(bitbucket.ownerAvatarURL.isEmpty, "Avatar url should be empty!")
         XCTAssertTrue(bitbucket.createdDate.isEmpty, "Create date should be empty!")
     }
